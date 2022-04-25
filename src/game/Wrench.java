731fd9e195f94977013c45681b8c5a7703ac1369
sjha0002 <sjha0002@student.monash.edu>
2022-04-25 16:12:19 +1000

    Attack dialogue now uses Weapon verb instead of just 'attacks'

--- src/game/Wrench.java
+++ src/game/Wrench.java
@@ -1,19 +1,19 @@
 package game;
 
 import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.weapons.Weapon;
 import edu.monash.fit2099.engine.weapons.WeaponItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Wrench extends WeaponItem {
+public class Wrench extends WeaponItem implements Weapon {
     private static List<Wrench> wrenchList = new ArrayList<Wrench>();
     /**
      * Constructor.
-
      */
     public Wrench() {
-        super("wrench", 'W', 50, "hits with wrench", 80);
+        super("wrench", 'W', 50, "uses wrench to hit", 80);
         wrenchList.add(this);
     }
 
