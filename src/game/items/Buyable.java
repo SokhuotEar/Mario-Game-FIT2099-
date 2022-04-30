ac0905dfe118bec8b66dbf6389c269951d20013e
Klarissa <kjut0001@student.monash.edu>
2022-05-01 03:05:27 +1000

    documentation for dirt and buyable

--- src/game/items/Buyable.java
+++ src/game/items/Buyable.java
@@ -2,6 +2,9 @@ package game.items;
 
 import edu.monash.fit2099.engine.items.Item;
 
+/**
+ * Interface buyable holds all the methods of the items that can be bought by player
+ */
 public interface Buyable {
     public int getPrice();
     public Item getItem();
--- src/game/positions/Dirt.java
+++ src/game/positions/Dirt.java
@@ -9,13 +9,26 @@ import java.util.List;
  * A class that represents bare dirt.
  */
 public class Dirt extends Ground {
+	/**
+	 * A list that contains the bare dirt
+	 */
 	private static final List<Ground> dirtList = new ArrayList<>();
 
+	/**
+	 * Constructor which create and add dirt to dirtList
+	 */
 	public Dirt() {
 		super('.');
 		dirtList.add(this);
 	}
 
+	/**
+	 * An instance that is used to check whether a specific ground is
+	 * within the dirtList
+	 *
+	 * @param ground the ground to be checked
+	 * @return boolean based on whether the ground is in dirtList
+	 */
 	public static boolean isInstance(Ground ground) {
 		return dirtList.contains(ground);
 	}
