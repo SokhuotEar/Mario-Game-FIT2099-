e988e6d759ceb8b7ae8338004266cb07f3eab06e
sjha0002 <sjha0002@student.monash.edu>
2022-05-21 22:47:20 +1000

    Removed Unused import

--- src/game/actions/DrinkAction.java
+++ src/game/actions/DrinkAction.java
@@ -3,7 +3,6 @@ package game.actions;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
-import game.actors.enemies.behaviours.FollowBehaviour;
 import game.items.Drinkable;
 import game.positions.Fountain;
 
